9f25c6e04c21628be3ef426d5158a7ac6beba772
MoonJaehyun <jjmjh214@gmail.com>
2023-03-28 21:50:04 +0900

    add dart datatypes

---
+++ dart/Data Types/Baisc-datatypes.dart
@@ -0,0 +1,12 @@
+/*
+여러가지 타입을 가질 수 있는 변수에 쓰는 키워드
+*/
+
+void main() {
+    String name = "tom";
+    bool isPlay = true;
+    int age = 10;
+    double money = 52.55;
+    num x = 12;
+    num y = 1.2;
+}
